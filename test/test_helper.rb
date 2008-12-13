ad9b0a41699afe33a5a9636932e815a96173998a
Noah Thorp <noahthorp@rix-8.local>
2008-12-13 08:26:33 -0800

    Moved test unit tests into specs.

--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-require "test/unit"
-# BIN_PATH = File.expand_path(File.dirname(__FILE__)) + "/../bin"
-# Dir.foreach(BIN_PATH) {|f| require BIN_PATH + "/" + f unless [".",".."].include?(f)}
-
-# load the rsound init file
-require File.expand_path(File.dirname(__FILE__)) + "/../init.rb"
